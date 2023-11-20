9fff30f670db2ab29a20c09b5f5d16d017990df7
ElaineNew <elaineqiu0531@gmail.com>
2023-11-20 11:15:11 -0500

    finalize goto chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 import { Button } from "@/components/ui/button"
 import { UserButton,auth } from "@clerk/nextjs"
 import  Link  from "next/link";
-import { LogIn } from "lucide-react";
+import { ArrowRight, LogIn } from "lucide-react";
 import FileUpload from "@/components/FileUpload";
+import { db } from "@/lib/db";
+import { chats } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
 export default async function Home() {
   const {userId} = await auth();
   const isAuth = !!userId;
+  let firstChat;
+  if (userId) {
+    firstChat = await db.select().from(chats).where(eq(chats.userId, userId));
+    if (firstChat) {
+      firstChat = firstChat[0];
+    }
+  }
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-red-200 via-red-300 to-yellow-200">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -15,9 +25,15 @@ export default async function Home() {
             <UserButton afterSignOutUrl="/" />
           </div>
           <div className="flex mt-2">
-            {isAuth &&
-            <Button>Go to Chats</Button>
-          }
+          {isAuth && firstChat && (
+              <>
+                <Link href={`/chat/${firstChat.id}`}>
+                  <Button>
+                    Go to Chats <ArrowRight className="ml-2" />
+                  </Button>
+                </Link>
+              </>
+            )}
           </div>
 
           <p className="max-w-xl mt-1 text-lg text-slate-600">
